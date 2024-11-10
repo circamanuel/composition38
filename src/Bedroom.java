2b04517ea21c9f4d16d842a8bf1b907472e9671a
circamanuel <manu4126@gmail.com>
2024-11-10 19:35:05 +0100

    final Code

---
+++ src/Bedroom.java
@@ -0,0 +1,31 @@
+public class Bedroom {
+
+    private String name;
+    public Wall wall1;
+    public Wall wall2;
+    public Wall wall3;
+    public Wall wall4;
+    public Ceiling ceiling;
+    public Bed bed;
+    public Lamp lamp;
+
+    public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp) {
+        this.name = name;
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.ceiling = ceiling;
+        this.bed = bed;
+        this.lamp = lamp;
+    }
+
+    public Lamp getLamp() {
+        return lamp;
+    }
+
+    public void makeBed() {
+        Bed.make();
+        System.out.print("Bed -> Making | ");
+    }
+}
---
+++ src/Ceiling.java
@@ -0,0 +1,18 @@
+public class Ceiling {
+
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
